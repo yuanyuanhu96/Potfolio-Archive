56cd9b27e4de0fcf665ba9b7a92b97a8299bb2e3
yuanyuanhu96 <yhu76@id.iit.edu>
2021-12-18 00:11:41 -0600

    updated css

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -2,10 +2,10 @@
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos >= currentScrollPos) {
-    document.getElementById('navbar').style.top = '0';
-  } else {
+  if (prevScrollpos < currentScrollPos) {
     document.getElementById('navbar').style.top = '-80px';
+  } else {
+    document.getElementById('navbar').style.top = '0';
   }
   prevScrollpos = currentScrollPos;
 };
